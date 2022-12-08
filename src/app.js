a3081be1dccf271565937ee725727679fa7a280a
Jacqueline M <75797321+rookiebehaviour@users.noreply.github.com>
2022-12-08 10:51:14 -0500

    Setup axios and added id's to main elements

--- src/app.js
+++ src/app.js
@@ -1,1 +1,10 @@
+let apiKey = "f10daba5t5ce3fc3ed35o46ebd038a42";
+let city = "Middletown";
+let unit = "metric";
+let url = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=${unit}`;
 
+function displayTemp(response) {
+  console.log(response.data.temperature.current);
+}
+
+axios.get(url).then(displayTemp);
