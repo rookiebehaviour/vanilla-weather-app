73557c587bd22af21edd6759239195f476a7fc2b
Jacqueline M <75797321+rookiebehaviour@users.noreply.github.com>
2022-12-08 15:43:36 -0500

    Added function to return date and time on front-end

--- src/app.js
+++ src/app.js
@@ -3,6 +3,31 @@ let city = "Middletown";
 let unit = "metric";
 let url = `https://api.shecodes.io/weather/v1/current?query=${city}&units=${unit}&key=${apiKey}`;
 
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  return `${day} ${hours}:${minutes}`;
+}
+
 function displayTemp(response) {
   console.log(response.data);
   let temperature = Math.round(response.data.temperature.current);
@@ -20,11 +45,14 @@ function displayTemp(response) {
   feelsLikeElement.innerHTML = `Feels like: ${feelsLike}°`;
 
   let humidityElement = document.querySelector("#humidity");
-  humidityElement.innerHTML = `Humidity: ${response.data.temperature.humidity}`;
+  humidityElement.innerHTML = `Humidity: ${response.data.temperature.humidity}%`;
 
   let wind = Math.round(response.data.wind.speed);
   let windSpeed = document.querySelector("#windspeed");
   windSpeed.innerHTML = `Wind: ${wind} km/h`;
+
+  let date = document.querySelector("#date");
+  date.innerHTML = formatDate(response.data.time * 1000);
 }
 
 axios.get(url).then(displayTemp);
