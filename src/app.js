26aea355b0ac78d3a1f9e8bdb99586c60d970f9a
Jacqueline M <75797321+rookiebehaviour@users.noreply.github.com>
2022-12-09 21:14:03 -0500

    Removed forecast HTML and added  afunction
    Created a displayForecast() function to display forecast and removed data from HTML to be automatically populated by the function via innerHTML

--- src/app.js
+++ src/app.js
@@ -28,6 +28,39 @@ function formatDate(timestamp) {
   return `${day} ${hours}:${minutes}`;
 }
 
+function displayForecast() {
+  let forecastElement = document.querySelector("#forecast");
+
+  let forecastHTML = `<div class="row">`;
+  let days = ["Thu", "Fri", "Sat", "Sun", "Mon", "Tue"];
+
+  days.forEach(function (day) {
+    forecastHTML =
+      forecastHTML +
+      `
+              <div class="col-2">
+                <div class="weather-forecast-date">
+                ${day}
+              </div>
+                <img src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/broken-clouds-day.png" 
+                alt="broken clouds day" 
+                width="42" />
+                <div class="weather-forecast-temperatures">
+                  <span class="weather-forecast-temperature-max">
+                    18°
+                  </span>
+                  <span class="weather-forecast-temperature-min">
+                    12°
+                  </span>
+                </div>
+              </div>
+            `;
+  });
+
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+}
+
 function displayTemp(response) {
   celciusTemp = response.data.temperature.current;
 
@@ -92,6 +125,7 @@ let celciusTemp = null;
 
 // Default search if no city entered
 search("Cincinnati");
+displayForecast();
 
 let form = document.querySelector("#search-form");
 form.addEventListener("submit", handleSubmit);
